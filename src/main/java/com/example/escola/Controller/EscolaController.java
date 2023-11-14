6138f905fa553c4ee06d10d16b7f1503d0aa7092
alexandrodasilva <alexsamdrosilva13@gmail.com>
2023-11-14 08:53:31 -0300

    refactory project

--- src/main/java/com/example/escola/Controller/EscolaController.java
+++ src/main/java/com/example/escola/Controller/EscolaController.java
@@ -1,16 +1,36 @@
 package com.example.escola.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/escola")
+import com.example.escola.models.Turma;
+import com.example.escola.service.TurmaService;
+
+import jakarta.validation.Valid;
+
+@RequestMapping("/schol")
 @RestController
 public class EscolaController {
+    final TurmaService turmaService;
     
+    //construtor
+    public EscolaController(TurmaService turmaService){
+        this.turmaService = turmaService;
+    }
+
     @GetMapping
     public String teste(){
         return "teste";
+    }
+    
+    @PostMapping
+    public ResponseEntity<Turma> savEntity(@Valid Turma turma){
+        var t = new Turma();
+        
+        return null;
     }
 }
