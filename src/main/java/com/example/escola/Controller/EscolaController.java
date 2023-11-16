7564462eb56a39e36c79c19d175a31cddeca7a9a
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2023-11-15 21:33:56 -0300

    refatoração do controller

--- src/main/java/com/example/escola/Controller/EscolaController.java
+++ src/main/java/com/example/escola/Controller/EscolaController.java
@@ -1,12 +1,7 @@
 package com.example.escola.Controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import com.example.escola.models.Turma;
 import com.example.escola.service.TurmaService;
@@ -17,19 +12,19 @@ import jakarta.validation.Valid;
 @RestController
 public class EscolaController {
     final TurmaService turmaService;
-    
-    //construtor
-    public EscolaController(TurmaService turmaService){
+
+    // construtor
+    public EscolaController(TurmaService turmaService) {
         this.turmaService = turmaService;
     }
 
     // @GetMapping
     // public String teste(){
-    //     return "teste";
+    // return "teste";
     // }
-    
+
     @PostMapping
-    public ResponseEntity<Object> saveEntity(@RequestBody @Valid Turma turma){
+    public ResponseEntity<Object> saveEntity(@RequestBody @Valid Turma turma) {
         return ResponseEntity.status(HttpStatus.CREATED).body(turmaService.save(turma));
     }
 }
