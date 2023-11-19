3b3c5e350ba48ca34fa8fbb4637a6a8f78ae7fba
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2023-11-19 14:21:14 -0300

    refatorado o classe controller para criar e listar turmas

--- src/main/java/com/example/escola/Controller/TurmaController.java
+++ src/main/java/com/example/escola/Controller/TurmaController.java
@@ -1,10 +1,7 @@
 package com.example.escola.Controller;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,45 +11,24 @@ import com.example.escola.service.TurmaService;
 import jakarta.validation.Valid;
 
 @RequestMapping("Turma")
-// @RestController
 @Controller
 public class TurmaController {
     final TurmaService turmaService;
 
-    // construtor
     public TurmaController(TurmaService turmaService) {
         this.turmaService = turmaService;
     }
 
-    // @PostMapping
-    // public ResponseEntity<Object> saveEntity(@RequestBody @Valid Turma turma) {
-    // return
-    // ResponseEntity.status(HttpStatus.CREATED).body(turmaService.save(turma));
-    // }
+    @PostMapping("/criar")
+    public String saveEntity(@ModelAttribute("turma") @Valid Turma turma) {
+        turmaService.save(turma);
+        return "redirect:/turma/turmaLista";
+    }
 
-    @GetMapping("/listar")
+    @GetMapping
     public String index(final Model model) {
         List<Turma> t = turmaService.findAll();
         model.addAttribute("t", t);
         return "turma/turmaLista";
     }
-
-    // Esse endopoint que retorna a operação HTTP GET, o ResponseEntity é o tipo de
-    // retorno do método, com o status OK(200)
-    // ResponseEntity representa a resposta HTTP do spring Framework, o
-    // status(HTTpStatus.OK) define o codigo de retorno OK(200) Sucesso, já o BODY,
-    // esse método é definido que o que retorna
-    // @GetMapping
-    // public ResponseEntity<List<Turma>> getAll() {
-    // return ResponseEntity.status(HttpStatus.OK).body(turmaService.findAll());
-    // }
-
-    // @GetMapping("/{id}")
-    // public ResponseEntity<Object> getID(@PathVariable @Valid Long id) {
-    // Optional<Turma> turma = turmaService.findById(id);
-    // if (!turma.isPresent()) {
-    // ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
-    // }
-    // return ResponseEntity.status(HttpStatus.OK).body(turma);
-    // }
 }
